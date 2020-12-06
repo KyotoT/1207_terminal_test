fdd341dfb8aff3e628760f6957ee13a3c17b4c68
KyotoT <57787316+KyotoT@users.noreply.github.com>
2020-12-07 01:50:51 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -64,3 +64,103 @@ $(function(){
     speed_vary:true,
   });
 });
+
+// Q4
+// ボックスを表示して、タイマーを開始
+function showBox4() {
+  document.getElementById("temporaryBox4").style.display = "block"; // ボックスを表示
+  timerId4 = setTimeout( closeBox4 , 5000 ); // タイマーを開始
+  document.getElementById("btnShowBox4").disabled = true; // 表示用ボタンを無効化
+}
+// ボックスを消して、タイマーを終了
+function closeBox4() {
+  document.getElementById("temporaryBox4").style.display = "none"; // ボックスを消す
+  clearTimeout( timerId4 ); // タイマーを終了
+  document.getElementById("btnShowBox4").disabled = false; // 表示用ボタンを有効化
+}
+
+$(function(){
+  $('#js-target4').t({
+    speed_vary:true,
+  });
+});
+
+// Q5
+// ボックスを表示して、タイマーを開始
+function showBox5() {
+  document.getElementById("temporaryBox5").style.display = "block"; // ボックスを表示
+  timerId5 = setTimeout( closeBox5 , 5000 ); // タイマーを開始
+  document.getElementById("btnShowBox5").disabled = true; // 表示用ボタンを無効化
+}
+// ボックスを消して、タイマーを終了
+function closeBox5() {
+  document.getElementById("temporaryBox5").style.display = "none"; // ボックスを消す
+  clearTimeout(timerId5); // タイマーを終了
+  document.getElementById("btnShowBox5").disabled = false; // 表示用ボタンを有効化
+}
+
+$(function(){
+  $('#js-target5').t({
+    speed_vary:true,
+  });
+});
+
+// Q6
+// ボックスを表示して、タイマーを開始
+function showBox6() {
+  document.getElementById("temporaryBox6").style.display = "block"; // ボックスを表示
+  timerId6 = setTimeout(closeBox6, 5000 ); // タイマーを開始
+  document.getElementById("btnShowBox6").disabled = true; // 表示用ボタンを無効化
+}
+// ボックスを消して、タイマーを終了
+function closeBox6() {
+  document.getElementById("temporaryBox6").style.display = "none"; // ボックスを消す
+  clearTimeout(timerId6); // タイマーを終了
+  document.getElementById("btnShowBox6").disabled = false; // 表示用ボタンを有効化
+}
+
+$(function(){
+  $('#js-target6').t({
+    speed_vary:true,
+  });
+});
+
+// Q7
+// ボックスを表示して、タイマーを開始
+function showBox7() {
+  document.getElementById("temporaryBox7").style.display = "block"; // ボックスを表示
+  timerId7 = setTimeout(closeBox7, 5000 ); // タイマーを開始
+  document.getElementById("btnShowBox7").disabled = true; // 表示用ボタンを無効化
+}
+// ボックスを消して、タイマーを終了
+function closeBox7() {
+  document.getElementById("temporaryBox7").style.display = "none"; // ボックスを消す
+  clearTimeout(timerId7); // タイマーを終了
+  document.getElementById("btnShowBox7").disabled = false; // 表示用ボタンを有効化
+}
+
+$(function(){
+  $('#js-target7').t({
+    speed_vary:true,
+  });
+});
+
+// Q8
+// ボックスを表示して、タイマーを開始
+function showBox8() {
+  document.getElementById("temporaryBox8").style.display = "block"; // ボックスを表示
+  timerId8 = setTimeout(closeBox8, 5000 ); // タイマーを開始
+  document.getElementById("btnShowBox8").disabled = true; // 表示用ボタンを無効化
+}
+// ボックスを消して、タイマーを終了
+function closeBox8() {
+  document.getElementById("temporaryBox8").style.display = "none"; // ボックスを消す
+  clearTimeout(timerId8); // タイマーを終了
+  document.getElementById("btnShowBox8").disabled = false; // 表示用ボタンを有効化
+}
+
+$(function(){
+  $('#js-target8').t({
+    speed_vary:true,
+  });
+});
